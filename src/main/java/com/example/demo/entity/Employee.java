8e84f888273a0e4735666219cd927113c7a87341
LamChu <lamchu@git.com>
2022-07-12 11:36:25 +0700

    first commit

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,74 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", length = 250)
+    private String name;
+
+    @Column(name = "email", length = 250)
+    private String email;
+
+    @Column(name = "address", length = 250)
+    private String address;
+
+    @Column(name = "phone", length = 250)
+    private String phone;
+
+    public Employee() {}
+
+    public Employee(Long id, String name, String email, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
